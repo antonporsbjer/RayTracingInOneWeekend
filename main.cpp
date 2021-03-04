4336af3da8ff26feb2f7ae71b6e0bdfc15d58c1f
porsbjer <porsbjer@kth.se>
2021-03-04 12:38:16 +0100

    added progress indicator

--- main.cpp
+++ main.cpp
@@ -10,6 +10,7 @@ int main(int argc, char **argv){
     std::cout << "P3\n" << image_width << " " << image_height << "\n255\n";
 
     for (int j = image_height-1; j >= 0; --j){
+        std::cerr << "\rScaning remaining: " << j << " " << std::flush;
         for(int i = 0; i < image_width; ++i){
             auto r = double(i) / (image_width-1);
             auto g = double(j) / (image_height-1);
@@ -22,4 +23,6 @@ int main(int argc, char **argv){
             std::cout << ir << " " << ig << " " << ib << "\n";
         }
     }
+
+    std::cerr << "\nDone.\n";
 }
