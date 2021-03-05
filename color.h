8bc0291a5ba80fdc5fcc69af65af7cca6aad43df
porsbjer <porsbjer@kth.se>
2021-03-05 11:19:10 +0100

    fixed floats + added ray scan

--- color.h
+++ color.h
@@ -5,9 +5,9 @@
 #include <iostream>
 
 void write_color(std::ostream &out, color pixel_color){
-    out << static_cast<int>(255.999 * pixel_color.x()) << " " 
-        << static_cast<int>(255.999 * pixel_color.y()) << " " 
-        << static_cast<int>(255.999 * pixel_color.z()) << "\n";
+    out << static_cast<int>(255.999f * pixel_color.x()) << " " 
+        << static_cast<int>(255.999f * pixel_color.y()) << " " 
+        << static_cast<int>(255.999f * pixel_color.z()) << "\n";
 }
 
 #endif
--- main.cpp
+++ main.cpp
@@ -1,12 +1,31 @@
 #include <iostream>
 #include "vec3.h"
 #include "color.h"
+#include "ray.h"
+
+color ray_color(const ray& r){
+    vec3 unit_direction = unit_vector(r.direction());
+    auto t = 0.5f * (unit_direction.y() + 1.0f);
+    
+    return (1.0f - t) * color(1.0f,1.0f,1.0f) + t * color(0.5f, 0.7f, 1.0f);
+}
 
 int main(int argc, char **argv){
 
     // Image size
-    const int image_width = 512;//256;
-    const int image_height = 512;//256;
+    const auto aspect_ratio = 16.0f / 9.0f;
+    const int image_width = 400;
+    const int image_height =static_cast<int>(image_width / aspect_ratio);
+
+    // Camera
+    auto viewport_height = 2.0f;
+    auto viewport_width = aspect_ratio * viewport_height;
+    auto focal_length = 1.0f;
+
+    auto origin = point3(0.0f, 0.0f, 0.0f);
+    auto horizontal = vec3(viewport_width, 0.0f, 0.0f);
+    auto vertical = vec3(0.0f, viewport_height, 0.0f);
+    auto lower_left_corner = origin - horizontal / 2.0f - vertical / 2.0f - vec3(0.0f, 0.0f, focal_length);
 
     // Render
     std::cout << "P3\n" << image_width << " " << image_height << "\n255\n";
@@ -14,7 +33,10 @@ int main(int argc, char **argv){
     for (int j = image_height-1; j >= 0; --j){
         std::cerr << "\rScaning remaining: " << j << " " << std::flush;
         for(int i = 0; i < image_width; ++i){
-            color pixel_color(float(i) / (image_width-1), float(j) / (image_height-1), 0.25);
+            auto u = float(i) / (image_width -1);
+            auto v = float(j) / (image_height -1);
+            ray r(origin, lower_left_corner + u * horizontal + v * vertical - origin);
+            color pixel_color = ray_color(r);
             write_color(std::cout, pixel_color);
         }
     }
